c3e4c857fdff6d2c9bb3f488d755f70fe3e587ec
Alex Levinson <gen0cide.threats@gmail.com>
2014-09-22 20:03:02 -0700

    fixed patch.sh

--- patch.sh
+++ patch.sh
@@ -1,3 +1,3 @@
-curl -o /opt/metasploit/apps/pro/msf3/lib/rex/post/meterpreter/ui/console/command_dispatcher/priv.rb 'path_to_priv.rb'
-curl -o /opt/metasploit/apps/pro/msf3/lib/rex/post/meterpreter/ui/console/command_dispatcher/priv/power_shell.rb 'path_to_power_shell.rb'
-
+curl -o /opt/metasploit/apps/pro/msf3/lib/rex/post/meterpreter/ui/console/command_dispatcher/priv.rb 'https://raw.githubusercontent.com/gen0cide-/sawed_off/master/priv.rb'
+curl -o /opt/metasploit/apps/pro/msf3/lib/rex/post/meterpreter/ui/console/command_dispatcher/priv/power_shell.rb 'https://raw.githubusercontent.com/gen0cide-/sawed_off/master/power_shell.rb'
+echo "[*] Patched."
